636768dcae237b6b26472a646d9fd0de71a3221b
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-18 12:56:50 -0600

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'haunted-black': '#1a1a1a',
+        'ghostly-white': '#e0e0e0',
+        'eerie-green': '#39FF14',
+      },
+    },
+  },
+  plugins: [],
+}
