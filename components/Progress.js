19420c9d5dffd48cbd44585d1f019399a5f6cc13
thestateofcybersecurity <98850458+thestateofcybersecurity@users.noreply.github.com>
2024-09-18 20:29:37 -0600

    Update Progress.js

--- components/Progress.js
+++ components/Progress.js
@@ -5,9 +5,9 @@ const Progress = ({ currentRoom, totalRooms }) => {
   const progress = ((currentRoom + 1) / totalRooms) * 100;
 
   return (
-    <div className="w-full bg-gray-700 rounded-full h-2.5 dark:bg-gray-700 mt-4">
+    <div className="w-full bg-gray-700 rounded-full h-4 dark:bg-gray-700">
       <motion.div 
-        className="bg-purple-600 h-2.5 rounded-full"
+        className="bg-purple-600 h-4 rounded-full"
         initial={{ width: 0 }}
         animate={{ width: `${progress}%` }}
         transition={{ duration: 0.5 }}
